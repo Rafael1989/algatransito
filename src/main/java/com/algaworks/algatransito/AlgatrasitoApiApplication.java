519485a4bee85cfba9b5a0cba5ff9041793d3f83
Rafael <flns_rafa@hotmail.com>
2026-03-19 18:07:32 +0100

    First commit

---
+++ src/main/java/com/algaworks/algatransito/AlgatrasitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algatransito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgatrasitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgatrasitoApiApplication.class, args);
+	}
+
+}
